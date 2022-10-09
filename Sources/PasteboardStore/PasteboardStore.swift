1efef39d6e556b45705a769b5d56b725429604a4
Mikkel Sindberg Eriksen <mikkelse@gmail.com>
2022-10-09 16:52:25 +0200

    Refactor to use pasteboard type

--- Sources/PasteboardStore/PasteboardStore.swift
+++ Sources/PasteboardStore/PasteboardStore.swift
@@ -1,35 +1,33 @@
 //
 //  PasteboardStore.swift
-//  Unleashd
 //
 //  Created by Mikkel Sindberg Eriksen on 07/06/2022.
 //
 
-import Foundation
 import UIKit
 
 /// A type that can read and write a payload to the pasteboard.
 public struct PasteboardStore<P: Codable> {
 
-    /// The identifier to use for referenceing the payload in the pasteboard.
-    public let identifier: String
+    /// The pasteboard type to use for storing the payload in the pasteboard. Usually, reverse domain name style.
+    public let pasteBoardType: String
 
-    /// Initialize the store with the given identifer.
-    public init(identifier: String) {
-        self.identifier = identifier
+    /// Initialize the store with the given pasteboard type to use for storing a payload in the pasteboard..
+    public init(pasteBoardType: String) {
+        self.pasteBoardType = pasteBoardType
     }
 
-    /// Read a payload associated with the store's identifier in the pasteboard.
-    /// - returns nil if no payload was found associated with the given identifier
+    /// Read a payload associated with the store's pasteboard type.
+    /// - returns nil if no payload was found for the pasteboard type of the store.
     /// - throws if a payload was found, but there was an issue de-serializing it from the pasteboard.
     public func readPayload() throws -> P? {
-        return try PasteboardStore.readPayload(for: identifier)
+        return try PasteboardStore.readPayload(for: pasteBoardType)
     }
 
-    /// Write a payload to the pasteboard, associating it with the store's identifier.
+    /// Write a payload to the pasteboard, using the store's pasteboard type.
     /// - throws if there was an issue writing the payload to the pasteboard.
     public func writePayload(_ payload: P) throws {
-        try PasteboardStore.write(payload, with: identifier)
+        try PasteboardStore.write(payload, with: pasteBoardType)
     }
 
     /// Clear the payload from the pasteboard.
@@ -40,22 +38,18 @@ public struct PasteboardStore<P: Codable> {
 
 extension PasteboardStore {
 
-    private static func write(_ payload: P, with identifier: String) throws {
-        let base64Payload = try payload.toData().base64EncodedString()
-        UIPasteboard.general.string = "\(base64Payload).\(identifier)"
+    private static func write(_ payload: P, with type: String) throws {
+        let payloadData = try payload.toData()
+        UIPasteboard.general.setData(payloadData, forPasteboardType: type)
     }
 
-    private static func readPayload(for identifier: String) throws -> P? {
-        guard
-            let content = UIPasteboard.general.string, content.contains(identifier),
-            let encodedPayload = content.components(separatedBy: ".").first,
-            let data = Data.init(base64Encoded: encodedPayload)
-        else { return nil }
-        return try P(from: data)
+    private static func readPayload(for type: String) throws -> P? {
+        guard let payloadData = UIPasteboard.general.data(forPasteboardType: type) else { return nil }
+        return try P(from: payloadData)
     }
 
     private static func clearPayload() {
-        UIPasteboard.general.string = ""
+        UIPasteboard.general.items = []
     }
 }
 
